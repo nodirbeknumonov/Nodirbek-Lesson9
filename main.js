7d9942596dba1b8fac7a0d6e439fe43f0f63c474
nodirbeknumonov <numonovnodirbek2007@gmail.com>
2023-03-18 06:36:34 +0500

    Lesson_9

---
+++ main.js
@@ -0,0 +1,25 @@
+function openNavbar() {
+    document.getElementById("navbar-responsive").style.left = "0";
+    // document.getElementById("navbar-responsive").style.top = "0";
+  }
+  function closeNavbar() {
+    document.getElementById("navbar-responsive").style.left = "-100%";
+    // document.getElementById("navbar-responsive").style.top = "-100%";
+  }
+  
+  document.getElementById("navbar-open").addEventListener("click", openNavbar);
+  document.getElementById("navbar-close").addEventListener("click", closeNavbar);
+
+  window.addEventListener("scroll", function () {
+    toggleBacktop();
+  });
+  
+  let backtop = document.getElementById("backtop");
+  
+  function toggleBacktop() {
+    if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
+      backtop.style.bottom = "50px";
+    } else {
+      backtop.style.bottom = "-50px";
+    }
+  }
